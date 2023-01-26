d5f51bef5477fa4b16d30493bde182118dafd091
Jakex26 <122539748+Jakex26@users.noreply.github.com>
2023-01-26 08:46:02 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    X += 1
+    basic.showNumber(X)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(0)
+    X = 0
+})
+input.onButtonPressed(Button.B, function () {
+    X += -1
+    basic.showNumber(X)
+})
+let X = 0
+X = 0
